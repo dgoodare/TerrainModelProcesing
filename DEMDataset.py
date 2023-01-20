b594f9d7e2f133fde97e17f8bc7f75d0d962e0ee
Duncan Goodare <dgoodare@dundee.ac.uk>
2023-01-20 16:05:53 +0000

    moved dataset class into separate file

---
+++ DEMDataset.py
@@ -0,0 +1,13 @@
+from torch.utils.data import Dataset
+
+
+class DEMDataset(Dataset):
+    """A modified version of the PyTorch Dataset class"""
+    def __init__(self):
+        return
+
+    def __len__(self):
+        return
+
+    def __getitem__(self, item):
+        return
