e8504dfd42309b7afd77bb7bd2383e9c9ed53d05
Duncan Goodare <dgoodare@dundee.ac.uk>
2023-02-02 18:57:49 +0000

    improved mask application process

--- DatasetManager.py
+++ DatasetManager.py
@@ -73,7 +73,7 @@ def CreateCircleMask(image, radius):
 
     # define the centre of the circle to be the centre of the image
     c_x, c_y = int(image.shape[0]/2), int(image.shape[1]/2)
-    r, c = disk((c_x, c_y), 10)
+    r, c = disk((c_x, c_y), radius)
     mask[r, c] = 0
 
     imageArray = np.multiply(image, mask)
@@ -117,6 +117,102 @@ def CreatePolygonMask(image):
     return image, mask
 
 
+def CreateTopLeftEdgeMask(image):
+    """Adds a polygon mask to an image"""
+    # create numpy array
+    mask = np.ones([image.shape[0], image.shape[1], 3], dtype=int)
+    # define coordinates for the vertices of the polygon
+    a = [0, 0]  # top left corner of the image
+    b = [int(2*image.shape[0]/10), 0]  # top edge
+    c = [0, int(3*image.shape[0]/10)]  # left edge
+    # define row coordinates
+    rows = np.array([a[1], b[1], c[1]])
+    # define column coordinates
+    cols = np.array([a[0], b[0], c[0]])
+    # create polygon
+    r, c = polygon(rows, cols)
+    # fill mask
+    mask[r, c] = 0
+
+    imageArray = np.multiply(image, mask)
+    image = Image.fromarray(np.uint8(imageArray))
+    return image, mask
+
+
+def CreateTopRightEdgeMask(image):
+    """Adds a polygon mask to an image"""
+    # create numpy array
+    mask = np.ones([image.shape[0], image.shape[1], 3], dtype=int)
+    # define coordinates for the vertices of the polygon
+    a = [image.shape[1]-1, 0]  # top left corner of the image
+    b = [int(8*image.shape[1]/10), 0]  # top edge
+    c = [image.shape[1]-1, int(1*image.shape[0]/10)]  # left edge
+    # define row coordinates
+    rows = np.array([a[1], b[1], c[1]])
+    # define column coordinates
+    cols = np.array([a[0], b[0], c[0]])
+    # create polygon
+    r, c = polygon(rows, cols)
+    # fill mask
+    mask[r, c] = 0
+
+    imageArray = np.multiply(image, mask)
+    image = Image.fromarray(np.uint8(imageArray))
+    return image, mask
+
+
+def CreateTopLeftStripMask(image):
+    """Adds a polygon mask to an image"""
+    # create numpy array
+    mask = np.ones([image.shape[0], image.shape[1], 3], dtype=int)
+
+    # define coordinates for the vertices of the polygon
+    a = [0, int(image.shape[0] * (2.5/10))]
+    b = [0, int(image.shape[0] * (3/10))]
+    c = [int(image.shape[1] * (3/10)), 0]
+    d = [int(image.shape[1] * (2.5/10)), 0]
+    # define column coordinates
+    cols = np.array([a[0], b[0], c[0], d[0]])
+    # define row coordinates
+    rows = np.array([a[1], b[1], c[1], d[1]])
+
+    # create polygon
+    r, c = polygon(rows, cols)
+
+    # fill mask
+    mask[r, c] = 0
+
+    imageArray = np.multiply(image, mask)
+    image = Image.fromarray(np.uint8(imageArray))
+    return image, mask
+
+
+def CreateBottomRightStripMask(image):
+    """Adds a polygon mask to an image"""
+    # create numpy array
+    mask = np.ones([image.shape[0], image.shape[1], 3], dtype=int)
+
+    # define coordinates for the vertices of the polygon
+    a = [image.shape[1]-1, int(image.shape[0] * (2/10))]
+    b = [image.shape[1]-1, int(image.shape[0] * (3/10))]
+    c = [int(image.shape[1] * (7/10)), image.shape[0]-1]
+    d = [int(image.shape[1] * (6.5/10)), image.shape[0]-1]
+    # define column coordinates
+    cols = np.array([a[0], b[0], c[0], d[0]])
+    # define row coordinates
+    rows = np.array([a[1], b[1], c[1], d[1]])
+
+    # create polygon
+    r, c = polygon(rows, cols)
+
+    # fill mask
+    mask[r, c] = 0
+
+    imageArray = np.multiply(image, mask)
+    image = Image.fromarray(np.uint8(imageArray))
+    return image, mask
+
+
 def saveImageToFile(outDir, filename, content):
     """Saves a PIL image object to a JPEG file"""
     try:
@@ -133,7 +229,7 @@ def saveMaskToFile(outDir, filename, content):
         print(f"{filename} could not be saved, or the file only contains partial data")
 
 
-def applyMasks(inputDir, imageOut, maskOut):
+def applyMasks(shapeList, inputDir, imageOut, maskOut):
     """ Applies selection of masks to all images in a given image directory and saves the resulting and the
         corresponding mask to files in separate output folders.
         Returns a list, where each item is a row to be written to a .csv file
@@ -157,27 +253,98 @@ def applyMasks(inputDir, imageOut, maskOut):
         # convert to numpy array
         imgArray = np.array(originalImg)
 
-        # add masks to images and store mask info
-        ellipseImage, ellipseMask = CreateEllipseMask(imgArray, 40, 20)
-        polyImage, polyMask = CreatePolygonMask(imgArray)
+        row = [img]
 
-        # create file names for images and masks and save to file
-        # images
-        ellipseImageFile = str(counter) + "_ellipseImage" + ".png"
-        saveImageToFile(imageOutDir, ellipseImageFile, ellipseImage)
+        # Top left edge
+        if shapeList[0]:
+            img, mask = CreateTopLeftEdgeMask(imgArray)
+            imgFile = "_tl_edge_img.png"
+            maskFile = "_tl_edge_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
 
-        polyImageFile = str(counter) + "polyImage" + ".png"
-        saveImageToFile(imageOutDir, polyImageFile, polyImage)
+        # Top right edge
+        if shapeList[1]:
+            img, mask = CreateTopRightEdgeMask(imgArray)
+            imgFile = "_tr_edge_img.png"
+            maskFile = "_tr_edge_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
 
-        # masks
-        ellipseMaskFile = str(counter) + "_ellipseMask" + ".npy"
-        saveMaskToFile(maskOutDir, ellipseMaskFile, ellipseMask)
+        # Top left strip
+        if shapeList[2]:
+            img, mask = CreateTopLeftStripMask(imgArray)
+            imgFile = "_tl_strip_img.png"
+            maskFile = "_tl_strip_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
 
-        polyMaskFile = str(counter) + "_polyMask" + ".npy"
-        saveMaskToFile(maskOutDir, polyMaskFile, polyMask)
+        # Bottom right strip
+        if shapeList[3]:
+            img, mask = CreateBottomRightStripMask(imgArray)
+            imgFile = "_br_strip_img.png"
+            maskFile = "_br_strip_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
 
-        # add file info to output list
-        row = [img, ellipseImageFile, ellipseMaskFile, polyImageFile, polyMaskFile]
+        # Square
+        if shapeList[4]:
+            img, mask = CreateSquareMask(imgArray, 16)
+            imgFile = "_sqr_img.png"
+            maskFile = "_sqr_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
+
+        # Centre strip
+        if shapeList[5]:
+            img, mask = CreateStripMask(imgArray, 16)
+            imgFile = "_c_strip_img.png"
+            maskFile = "_c_strip_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
+
+        # Circle
+        if shapeList[6]:
+            img, mask = CreateCircleMask(imgArray, 16)
+            imgFile = "_circle_img.png"
+            maskFile = "_circle_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
+
+        # Ellipse
+        if shapeList[7]:
+            img, mask = CreateEllipseMask(imgArray, 25, 16)
+            imgFile = "_ellipse_img.png"
+            maskFile = "_ellipse_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
+
+        # Polygon
+        if shapeList[8]:
+            img, mask = CreatePolygonMask(imgArray)
+            imgFile = "_poly_img.png"
+            maskFile = "_poly_mask.npy"
+            saveImageToFile(imageOutDir, str(counter) + imgFile, img)
+            saveMaskToFile(maskOutDir, str(counter) + maskFile, mask)
+            row.append(imgFile)
+            row.append(maskFile)
+
         outputList.append(row)
 
         # increment counter
@@ -187,16 +354,75 @@ def applyMasks(inputDir, imageOut, maskOut):
     return outputList
 
 
+def createCSVHeader(shapeList):
+    fields = ['Original Image']
+
+    if shapeList[0]:
+        fields.append('tl_edge img')
+        fields.append('tl_edge mask')
+
+    if shapeList[1]:
+        fields.append('tr_edge img')
+        fields.append('tr_edge mask')
+
+    if shapeList[2]:
+        fields.append('tl_strip img')
+        fields.append('tl_strip mask')
+
+    if shapeList[3]:
+        fields.append('br_strip img')
+        fields.append('br_strip mask')
+
+    if shapeList[4]:
+        fields.append('sqr img')
+        fields.append('sqr mask')
+
+    if shapeList[5]:
+        fields.append('c_strip img')
+        fields.append('c_strip mask')
+
+    if shapeList[6]:
+        fields.append('circle img')
+        fields.append('circle mask')
+
+    if shapeList[7]:
+        fields.append('ellipse img')
+        fields.append('ellipse mask')
+
+    if shapeList[8]:
+        fields.append('poly img')
+        fields.append('poly mask')
+
+    print(f"Fields: {fields}")
+
+    return fields
+
+
 def createLookUp():
-    csvRows = applyMasks('slicedImages', 'maskedImages/', 'outputMasks/')
+    # list of mask shapes to use
+    shapes = [
+        True,  # tl edge
+        True,  # tr edge
+        True,  # tl strip
+        True,  # br strip
+        False,  # square
+        True,  # centre strip
+        False,  # circle
+        False,  # ellipse
+        False  # polygon
+    ]
+
+    numShapes = 0
+    for shape in shapes:
+        if shape:
+            numShapes += 1
+
+    csvRows = applyMasks(shapes, 'slicedImages', 'maskedImages/', 'outputMasks/')
     # csv filename
     csvFile = 'LookUp/lookUpTable.csv'
+
     # column names for csv file
-    csvFields = ['Original image',
-                 'Ellipse image',
-                 'Ellipse mask',
-                 'Polygon image',
-                 'Polygon mask']
+    csvFields = createCSVHeader(shapes)
 
     try:
         # write to csv file
@@ -209,12 +435,16 @@ def createLookUp():
     except OSError:
         print("Failed to create lookUpTable.csv")
 
+    f = open("LookUp/lookUpTable.csv")
+    numSamples = len(f.readlines()) * numShapes
+    print(f"Number of samples: {numSamples}")
+
 
 def main():
     startTime = time.time()
     inputDir = 'inputImages'
     outputDir = 'slicedImages'
-    tileSize = 256
+    tileSize = 64
 
     # slice original images into smaller squares
     for img in os.listdir(inputDir):
@@ -226,5 +456,14 @@ def main():
     print(f"Dataset created in {time.time()-startTime} seconds")
 
 
-main()
+def testMask():
+    originalImg = Image.open('inputImages/image1.jpeg')
+    # convert to numpy array
+    imgArray = np.array(originalImg)
+    image, mask = CreateTopLeftStripMask(imgArray)
 
+    saveImageToFile('maskedImages/', "testImage.png", image)
+    saveMaskToFile('outputMasks/', "testMask.npy", mask)
+
+
+main()
