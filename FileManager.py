7659ad51c93f669859192dd6eeaf12a618ec2b0d
DuncanGoodare <duncangoodare@gmail.com>
2023-03-21 18:35:16 +0000

    saving and sorting trained models

---
+++ FileManager.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from shutil import rmtree
+import torch
+from datetime import datetime
+import os
+
+
+def CleanLogs():
+    """Remove old log data"""
+    for path in Path("logs").glob("**/*"):
+        if path.is_file():
+            path.unlink()
+        elif path.is_dir():
+            rmtree(path)
+
+
+def SaveModel(obj, directory, fileName):
+    """Save a trained model in the specified directory"""
+    path = directory + '/' + fileName
+    torch.save(obj, path)
+
+
+def CreateModelDir():
+    """Creates a new directory for saving trained models"""
+    now = datetime.now()
+    date = now.strftime("%d-%m-%Y")
+    time = now.strftime("%H-%M")
+    dirName = date + '_' + time
+    path = 'models/' + dirName
+    # create directory
+    try:
+        os.mkdir(path)
+    except OSError as e:
+        print(e)
+
+    return path
